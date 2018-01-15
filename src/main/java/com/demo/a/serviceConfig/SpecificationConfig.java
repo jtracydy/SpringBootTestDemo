99713ac54896fa11c308a14f3f464b5c3df9cc51
dan.wang <dan.wagn@msxf.com>
2018-01-15 19:20:38 +0800

    2018-01-15-19-20

--- src/main/java/com/demo/a/serviceConfig/SpecificationConfig.java
+++ src/main/java/com/demo/a/serviceConfig/SpecificationConfig.java
@@ -1,5 +1,6 @@
 package com.demo.a.serviceConfig;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,8 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -16,7 +19,13 @@ public class SpecificationConfig<T>{
         return new Specification() {
             @Override
             public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder cb) {
-                return null;
+                /**
+                 * 1 Root ORM对象和数据表对应，
+                 * 2 CriteriaBuilder 相当于SQL的where条件
+                 * 3 CriteriaQuery是一个顶层的查询，它包含查询的各个部分select 、from、where、group by、order by
+                 */
+                List<Predicate> predicates = new ArrayList<Predicate>();
+                return query.where(predicates.toArray(new Predicate[predicates.size()])).getRestriction();
             }
         };
     }
