637b6adaafaa4ccabd44eb19f6e848d754afb2d4
wangdan <jtracydy@163.com>
2018-01-17 23:21:45 +0800

    2018-01-18-23-21

--- src/main/java/com/demo/a/repository/TeacherRepository.java
+++ src/main/java/com/demo/a/repository/TeacherRepository.java
@@ -3,8 +3,13 @@ package com.demo.a.repository;
 import com.demo.a.entity.TeacherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface TeacherRepository extends JpaSpecificationExecutor<TeacherEntity>,JpaRepository<TeacherEntity,Long> {
 
+    @Query(value = "select t from TeacherEntity t")
+    List<TeacherEntity> getTeacherList();
 }
