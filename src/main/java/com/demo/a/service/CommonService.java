4c6bb132f2a5676e75c8c3febc27910744d521ff
dan.wang <dan.wagn@msxf.com>
2018-01-19 17:46:41 +0800

    2018-01-19-17-46

---
+++ src/main/java/com/demo/a/service/CommonService.java
@@ -0,0 +1,9 @@
+package com.demo.a.service;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public interface CommonService<T> {
+
+}
---
+++ src/main/java/com/demo/a/service/impl/CommonServiceImpl.java
@@ -0,0 +1,8 @@
+package com.demo.a.service.impl;
+
+import com.demo.a.service.CommonService;
+
+
+public class CommonServiceImpl<T> implements CommonService{
+
+}
