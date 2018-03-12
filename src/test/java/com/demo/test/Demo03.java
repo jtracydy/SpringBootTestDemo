60c1a486995b7c5f3bb4bdb0cda357c67b2cccd7
dan.wang <dan.wagn@msxf.com>
2018-03-12 19:36:55 +0800

    2018-03-12-19-35

--- src/test/java/com/demo/test/Demo03.java
+++ src/test/java/com/demo/test/Demo03.java
@@ -10,6 +10,7 @@ public class Demo03 {
     public static void main(String[] args) {
         Map<String,String> map = new HashMap<>();
         testMap(map);
+        testMap2(map);
         int p = 0;
         testInt(p);
         System.out.println(JSON.toJSONString(map));
@@ -18,6 +19,11 @@ public class Demo03 {
     private static void testMap(Map<String,String> map){
 
         map.put("123","123");
+    }
+
+    private static void testMap2(Map<String,String> map){
+
+        map.put("123","456");
     }
     private static void testInt(int p){
         p =10;
