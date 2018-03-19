510a39e447ba0adf8c20ae68b1ce624bbd5f9b9e
wangdan <jtracydy@163.com>
2018-03-19 23:34:37 +0800

    2018-03-19-23-34

--- src/main/java/com/demo/w/proxy/own/ProxyCglib.java
+++ src/main/java/com/demo/w/proxy/own/ProxyCglib.java
@@ -10,11 +10,25 @@ public class ProxyCglib implements MethodInterceptor {
 
     private Object target;
 
+    /**
+     * 创建代理类的代理方法
+     * @param zlass 被代理类的.class对象
+     * @param <T>
+     * @return
+     */
     public <T> T getProxy(Class<T> zlass){
         return (T)Enhancer.create(zlass,this);
     }
 
-
+    /**
+     * 调用代理类的方法
+     * @param o 代理对象
+     * @param method 被代理类的方法对象
+     * @param objects 调用代理方法的参数
+     * @param methodProxy 代理类方法对象
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         Object obj = methodProxy.invokeSuper(o,objects);
