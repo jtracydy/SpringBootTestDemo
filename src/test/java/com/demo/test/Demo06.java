9027c35f1828d7707bafed91c3b917d4444a4a4d
wangdan <jtracydy@163.com>
2018-04-01 14:55:14 +0800

    2018-04-01-14-54

---
+++ src/test/java/com/demo/test/Demo06.java
@@ -0,0 +1,23 @@
+package com.demo.test;
+
+public class Demo06 {
+
+
+    public static final int a = 4;
+
+    public static void main(String[] args) {
+
+        System.out.println(a);
+
+
+        Integer i1 = 100;
+        Integer i2 = 100;
+        Integer i3 = 200;
+        Integer i4 = 200;
+
+        System.out.println(i1==i2);
+        System.out.println(i3==i4);
+    }
+
+
+}
