7429296712ddef662c918c8ab8e97e7e2c4638f1
dan.wang <dan.wagn@msxf.com>
2018-04-24 16:38:14 +0800

    2018-04-24-16-21

---
+++ src/test/java/com/demo/test/Demo09.java
@@ -0,0 +1,18 @@
+package com.demo.test;
+
+public class Demo09 {
+
+
+    public static void main(String[] args) {
+
+        String s1 = "hello";
+
+        String s2 = "h";
+        String s3 = "ello";
+        String s4 = s2+s3 ;
+
+        System.out.println(s4 == s1);
+        System.out.println();
+
+    }
+}
